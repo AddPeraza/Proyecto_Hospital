76963ab6288319e5be4659f407e8d1e9164145ed
NepoPc <NepoPc@DESKTOP-L24LCET>
2024-07-31 19:42:00 -0700

    Add project files.

---
+++ Proyecto_Hospital/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Proyecto_Hospital
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(413, 129);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 20);
+            this.txtUser.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(413, 197);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 1;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(228, 136);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(228, 200);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(61, 13);
+            this.lblPass.TabIndex = 3;
+            this.lblPass.Text = "Contraseña";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(413, 252);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 700);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Name = "Form1";
+            this.Text = "Nombre";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ Proyecto_Hospital/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Hospital
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            String username = txtUser.Text;
+            String pass = txtPass.Text;
+
+            if(username == "Adderly" && pass == "123")
+            {
+                //MessageBox.Show("Acceso Correcto");
+                this.Hide();
+                Dashboard ds = new Dashboard();
+                ds.Show();
+            }
+            else
+            {
+                MessageBox.Show("Usuario/Contraseña Incorrectos");
+            }
+
+        }
+    }
+}
